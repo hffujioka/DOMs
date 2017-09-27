14e1d04846b888461570f5d871ff16b71835f4e8
hffujioka <30875167+hffujioka@users.noreply.github.com>
2017-09-27 08:53:26 -1000

    Add files via upload

---
+++ mousedown.js
@@ -0,0 +1,8 @@
+//This is my JS file//
+function showAnswer() {
+  document.getElementById('answer').style.visibility = "visible";
+}
+
+function hideAnswer() {
+  document.getElementById('answer').style.visibility = "hidden";
+}
---
+++ mousemove.js
@@ -0,0 +1,7 @@
+//This is my JS file//
+var i = 0;
+
+function runCounter() {
+  i++;
+  document.getElementById('theCounter').innerHTML = i;
+}
